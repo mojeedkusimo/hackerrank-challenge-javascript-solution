02d7d0817b8101b80a59d2667cf50c0683b2fcdc
mojeedkusimo <mkusimo90@gmail.com>
2023-09-28 17:15:07 +0100

    Complete Solution: Taum and B'day Challenge

--- Implementation/taumAndBday.js
+++ Implementation/taumAndBday.js
@@ -14,6 +14,8 @@ function taumBday(b, w, bc, wc, z) {
 
     if the unit cost of one color plus the additional cost of painting is lesser than the unit cost of the second color, all the gifts would be bought with the lesser color cost (and painted) otherwise the gifts are bought separately at the cost of each individual color.
 
+    For the first time, I learnt about the importance of large numbers and how Javascript handles them
+
     */
 
     let costOfBlack;
@@ -21,20 +23,32 @@ function taumBday(b, w, bc, wc, z) {
 
     const calcGiftCost = (cb, cw) => {
 
+        if (cb || cw > Number.MAX_SAFE_INTEGER) {
+
+            return BigInt(cb) + BigInt(cw);
+        }
+        
         return cb + cw;
+    }
+    const getProduct = (num1, num2) => {
+        if ( (num1 * num2) > Number.MAX_SAFE_INTEGER) {
+            let pdt = BigInt(num1) * BigInt(num2);
+            return pdt;
+        }
+        return num1*num2;
     }
 
     if ( bc > wc + z ) {
-        costOfBlack = b*(wc + z);
-        costOfWhite = w*wc;
+        costOfBlack = getProduct(b,(wc + z));
+        costOfWhite = getProduct(w,wc);
 
     } else if ( wc > bc + z ) {
-        costOfBlack = b*bc;
-        costOfWhite = w*(bc + z);
+        costOfBlack = getProduct(b,bc);
+        costOfWhite = getProduct(w,(bc + z));
 
     } else {
-        costOfBlack = b*bc;
-        costOfWhite = w*wc;
+        costOfBlack = getProduct(b,bc);
+        costOfWhite = getProduct(w,wc);
     }
 
     let giftCost = calcGiftCost(costOfBlack,costOfWhite);
@@ -50,3 +64,5 @@ taumBday(3, 6, 9, 1, 1); // 12
 taumBday(7, 7, 4, 2, 1); // 35
 taumBday(3, 3, 1, 9, 2); // 12
 taumBday(3, 5, 3, 4, 1); // 29
+taumBday(742407782 , 90529439, 847666641, 8651519 , 821801924); //  617318315833461267
+taumBday(142640749 , 652432197, 701695848 , 936714099  , 324221606); //  711232858900355655
