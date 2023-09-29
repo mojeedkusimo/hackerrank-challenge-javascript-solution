1997c40d648073705673185a98e54a09af7da946
mojeedkusimo <mkusimo90@gmail.com>
2023-09-29 09:37:04 +0100

    Complete Solution: Modified Kaprekar Numbers

--- Implementation/modifiedKaprekarNumbers.js
+++ Implementation/modifiedKaprekarNumbers.js
@@ -1,12 +1,20 @@
 function kaprekarNumbers(p, q) {
 
     /*
-    
+
     One needs to be cautious of these 80-90% success rate. Anyways, we move...
 
     Basically, we are just checking for a modified Kaprekar number in each range provided.
 
     It is safe to assume that any number square yields an odd-numbered digits would not be a modified Kaprekar.
+
+    Unfortunately, the assumption does not hold.
+
+    The logic of splitting the number into two equal halves is therefore invalid.
+
+    A more generic approach would be to pick each digit of a number at a time and sum it up with the remaining digits to see if satisfy the modified kaprekar condition.
+
+    Another point to consider is the slight differnce with the original Kaprekar Number definition in which The right hand part must be numbered digits long.
 
     */
    
@@ -28,8 +36,14 @@ function kaprekarNumbers(p, q) {
         return 0;
     }
 
+    if ( p <= 1 ) {
+
+        kaprekarArray.push(1);
+    }
+
     for (let i = p; i < q + 1; i++) {
 
+        const strNumInteger = `${i}`;
         const strNum = `${i * i}`;
         let strNumArray = strNum.split('');
 
@@ -41,11 +55,11 @@ function kaprekarNumbers(p, q) {
             let rightDigArray = strNumArray.slice(j+1,);
             let rightDig = convertArrayToNum(rightDigArray);
 
-            if ( j === 0 &&  rightDigArray.length > 1 && rightDig === 0) {
+            if ( j === 0 &&  rightDigArray.length > 1 && rightDig === 0 ) {
                 invalidNums.push(i);
             }
 
-            if (Number(leftDig) + Number(rightDig) === i && !invalidNums.includes(i)) {
+            if (Number(leftDig) + Number(rightDig) === i && !invalidNums.includes(i) && rightDigArray.length === strNumInteger.length ) {
                 kaprekarArray.push(i);
             }
         }
@@ -63,5 +77,7 @@ function kaprekarNumbers(p, q) {
     return;
 }
 
-kaprekarNumbers(1, 100000);
+// kaprekarNumbers(1, 100);
 // kaprekarNumbers(100, 300);
+kaprekarNumbers(1, 100000); //4879,5292,38962
+// kaprekarNumbers(1, 99999); 
